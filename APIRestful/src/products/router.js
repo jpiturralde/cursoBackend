b2396c84c2e6568da54a1682c810c582fb68fc41
Juan Iturralde <jpiturralde@gmail.com>
2021-09-27 12:19:40 -0300

    Reestrucuro common.js. Agrego scripts en package.json. Arreglo index.html.

--- APIRestful/src/products/router.js
+++ APIRestful/src/products/router.js
@@ -1,10 +1,19 @@
 const DB = require('./products-inMemory-db.js')
 const { Router } = require('express')
-const { idValidator } = require('../common/common.js')
 
 const productsRouter = new Router()
 
-productsRouter.get('/', (req, res) => {
+const ERR_400_INVALID_ID = { error: 'El parámetro ingresado no es un número' }
+const idValidator = (req, res, next) => {
+    const num = parseInt(req.params.id)
+
+    if (isNaN(num)) {
+        return res.status(400).json(ERR_400_INVALID_ID)
+    }
+    next();
+}
+
+productsRouter.get('/', async (req, res) => {
     res.json(DB.get())
 })
 
@@ -21,13 +30,13 @@ productsRouter.get('/:id',
     }
 )
 
-productsRouter.post('/', (req, res) => {
+productsRouter.post('/', async (req, res) => {
     res.status(201).json(DB.post(req.body))
 })
 
 productsRouter.put('/:id', 
     idValidator, 
-    (req, res) => {
+    async (req, res) => {
         const response = DB.put(parseInt(req.params.id), req.body)
         if (!response) {
             res.status(204).json()
@@ -38,7 +47,7 @@ productsRouter.put('/:id',
 
 productsRouter.delete('/:id', 
     idValidator, 
-    (req, res) => {
+    async (req, res) => {
         DB.remove(parseInt(req.params.id))
         res.json()
     }
