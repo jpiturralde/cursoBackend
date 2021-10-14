e3fb434a7b50e322a2b898c7d1f50863d0132873
Juan Iturralde <jpiturralde@gmail.com>
2021-10-14 13:32:43 -0300

    Vista de productos andando sin persistencia en archivos.

--- WebSockets/public/js/main.js
+++ WebSockets/public/js/main.js
@@ -10,7 +10,31 @@ async function loadAndCompileTemplate(templatePath) {
     return Handlebars.compile(template)
 }
 
-async function render(messages) {
+/* BEGIN PRODUCTS */
+async function renderProducts(products) {
+    console.log(products)
+    const productsTpl = await loadAndCompileTemplate('/templates/products.hbs')
+    const thereAreProducts = products.length > 0
+    const html = productsTpl({ thereAreProducts, products })
+    document.getElementById('products').innerHTML = html
+}
+
+function addProduct(e) {
+    const product = {
+        title: document.getElementById('title').value,
+        price: document.getElementById('price').value,
+        thumbnail: document.getElementById('thumbnail').value
+    };
+    document.getElementById('title').value = ''
+    document.getElementById('price').value = ''
+    document.getElementById('thumbnail').value = ''
+    socket.emit('new-product', product);
+    return false;
+}
+/* END PRODUCTS */
+
+/* BEGIN MESSAGES */
+async function renderMessages(messages) {
     const messagesTpl = await loadAndCompileTemplate('/templates/messages.hbs')
     const html = messagesTpl({ messages })
     document.getElementById('messages').innerHTML = html
@@ -26,6 +50,7 @@ function addMessage(e) {
     socket.emit('new-message', message);
     return false;
 }
+/* END MESSAGES */
 
-
-socket.on('messages', function(messages) { render(messages); });
+socket.on('messages', function(messages) { renderMessages(messages); });
+socket.on('products', function(products) { renderProducts(products); });
--- WebSockets/src/server.js
+++ WebSockets/src/server.js
@@ -10,8 +10,34 @@ const messages = [
     { author: "Juan", text: "¡Hola! ¿Que tal?", ts: 1634218734271},
     { author: "Pedro", text: "¡Muy bien! ¿Y vos?", ts: 1634219290446 },
     { author: "Ana", text: "¡Genial!", ts: 1634219329945 }
- ];
+];
 
+const products = [
+    {
+      id: 1,
+      value: {
+        title: "Escuadra",
+        price: 122.45,
+        thumbnail: "https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png"
+      }
+    },
+    {
+      id: 2,
+      value: {
+        title: "Calculadora",
+        price: 234.56,
+        thumbnail: "https://cdn3.iconfinder.com/data/icons/education-209/64/calculator-math-tool-school-256.png"
+      }
+    },
+    {
+      id: 3,
+      value: {
+        title: "Globo Terráqueo",
+        price: 345.67,
+        thumbnail: "https://cdn3.iconfinder.com/data/icons/education-209/64/globe-earth-geograhy-planet-school-256.png"
+      }
+    }
+  ]
 
 app.use(express.static('public'))
 
@@ -21,10 +47,20 @@ io.on('connection', socket => {
     /* Envio los mensajes al cliente que se conectó */
     socket.emit('messages', messages)
 
+    /* Envio los productos al cliente que se conectó */
+    socket.emit('products', products)
+    
     socket.on('new-message',data => {
         data.ts = Date.now()
         messages.push(data);
         io.sockets.emit('messages', messages);
+    });
+
+    socket.on('new-product',data => {
+        console.log(data)
+        const p = { id: products.length, value: data}
+        products.push(p);
+        io.sockets.emit('products', products);
     });
 })
 
