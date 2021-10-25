f547bb5bda647389dcb06862ef4d1aa1aec212c2
Juan Iturralde <jpiturralde@gmail.com>
2021-10-24 22:03:22 -0300

    Agrego comentario.

--- ProyectoFinal/src/lib/unknown-route-middleware.js
+++ ProyectoFinal/src/lib/unknown-route-middleware.js
@@ -1,5 +1,8 @@
+/*
+En el caso de requerir una ruta no implementada en el servidor, este debe contestar un objeto de error.
+ej { error : -2, descripcion: ruta 'x' método 'y' no implementada}
+*/
 export const unkownRoute = (req, res) => {
-    // Invalid request
           res.status(404).json(UNKNOWN_ROUTE(req));
     }
 
