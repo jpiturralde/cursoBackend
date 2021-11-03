865f65b32248c72fca11a8f99ee00b53d256e806
Juan Iturralde <jpiturralde@gmail.com>
2021-11-03 19:51:16 -0300

    Elimino creación de tabla messages.

--- WebSockets/scripts/websocketsdb_ddl.sql
+++ WebSockets/scripts/websocketsdb_ddl.sql
@@ -16,13 +16,3 @@ CREATE TABLE products(
     thumbnail VARCHAR(250) NOT NULL,
     primary key(id)
 );
-
-DROP TABLE messages;
-
-CREATE TABLE messages(
-	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    ts BIGINT UNSIGNED NOT NULL,
-    author VARCHAR(50) NOT NULL,
-    msg VARCHAR(250),
-    primary key(id)
-);
