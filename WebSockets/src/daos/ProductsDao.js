cdb3be2545c8516ee9e0c137b0469aaac95dcd39
Juan Iturralde <jpiturralde@gmail.com>
2022-01-13 09:41:57 -0300

    Limpio código.

--- WebSockets/src/daos/ProductsDao.js
+++ WebSockets/src/daos/ProductsDao.js
@@ -27,40 +27,4 @@ export default class ProductsDao extends Dao {
         }
         return await this.#repo.post(data)
     }
-    // schemaErrors(data) {
-    //     const errors = this.schemaValidations(data)
-
-    //     if (errors.length > 0) {
-    //         throw new Error(errors)
-    //     }
-    // }
-
-    // schemaValidations(data) {
-    //     const errors = []
-    //     if (!data.name) {
-    //         errors.push('Producto: Falta campo name')
-    //     }
-    //     if (!data.code) {
-    //         errors.push('Producto: Falta campo code')
-    //     }
-    //     if (isNaN(parseInt(data.price)) || data.price < 0) {
-    //         errors.push('Producto: Campo price inválido')
-    //     }
-    //     if (isNaN(parseInt(data.stock)) || data.stock < 0) {
-    //         errors.push('Producto: Campo stock inválido')
-    //     }
-    //     return errors
-    // }
-
-    // async post(data) { 
-    //     this.schemaErrors(data)
-
-    //     const content = await this.#repo.getAll()
-    //     const exists = content.filter(x => x.code == data.code)
-    //     if (exists.length > 0) {
-    //         return exists[0]
-    //     }
-
-    //     return this.#repo.post(Dao.createPayload(data))
-    // }
 }
