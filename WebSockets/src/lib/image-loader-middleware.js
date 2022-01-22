6848becf594ca2a2e8d11b96f2eb6f499adc7693
Juan Iturralde <jpiturralde@gmail.com>
2022-01-22 14:38:09 -0300

    Elimino imgUri porque no llega al objeto file.

--- WebSockets/src/lib/image-loader-middleware.js
+++ WebSockets/src/lib/image-loader-middleware.js
@@ -2,9 +2,6 @@ import multer from 'multer'
 
 export const imageLoaderMdw = (imagePath) => {
     const storage = multer.diskStorage({
-        imgUri: function (req, file, cb) {
-            cb(null, '/views/images')
-          },
         destination: function (req, file, cb) {
           cb(null, imagePath)
         },
