06228f8a633a257df709ad57d559e16ff044d6bd
Juan Iturralde <jpiturralde@gmail.com>
2022-03-06 19:03:57 -0300

    Corrijo delete.

--- ProyectoFinal/src/api/products.js
+++ ProyectoFinal/src/api/products.js
@@ -3,7 +3,7 @@ const apiSpec = (ds) => {
         getById: async (id) => { return await ds.getById(id) },
         get: async () => { return await ds.get() },
         post: async (data) => { return await ds.post(data) },
-        deleteById: async (id) => { return await ds.getById(id) }
+        deleteById: async (id) => { return await ds.deleteById(id) }
     }
 }
 
